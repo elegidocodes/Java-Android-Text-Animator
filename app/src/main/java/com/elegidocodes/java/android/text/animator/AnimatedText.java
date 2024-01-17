9773a9be459a17224f3c809338e354c8f6014f59
elegidocodes <fernandoismaelcaballero@gmail.com>
2024-01-17 10:20:13 -0600

    Comments for AnimatedText.java

--- app/src/main/java/com/elegidocodes/java/android/text/animator/AnimatedText.java
+++ app/src/main/java/com/elegidocodes/java/android/text/animator/AnimatedText.java
@@ -8,22 +8,49 @@ import android.util.Log;
 
 import androidx.annotation.Nullable;
 
+/**
+ * Custom TextView subclass that provides the ability to animate text content.
+ * This class extends androidx.appcompat.widget.AppCompatTextView.
+ */
 public class AnimatedText extends androidx.appcompat.widget.AppCompatTextView {
 
     private ValueAnimator textAnimator; // Reference to the animator
 
+    /**
+     * Constructor for creating an AnimatedText instance.
+     *
+     * @param context The context in which this view will be used.
+     */
     public AnimatedText(Context context) {
         super(context);
     }
 
+    /**
+     * Constructor for creating an AnimatedText instance with attributes.
+     *
+     * @param context The context in which this view will be used.
+     * @param attrs   The attribute set containing the view's attributes.
+     */
     public AnimatedText(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
 
+    /**
+     * Constructor for creating an AnimatedText instance with attributes and a style.
+     *
+     * @param context      The context in which this view will be used.
+     * @param attrs        The attribute set containing the view's attributes.
+     * @param defStyleAttr An attribute from the current theme.
+     */
     public AnimatedText(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
+    /**
+     * Sets up scroll animation for a single line of text.
+     *
+     * @param repeatCount The number of times the animation should repeat.
+     */
     public void setScrollAnimationInSingleLine(int repeatCount) {
         this.setSelected(true);
         this.setSingleLine(true);
@@ -31,6 +58,13 @@ public class AnimatedText extends androidx.appcompat.widget.AppCompatTextView {
         this.setMarqueeRepeatLimit(repeatCount);
     }
 
+    /**
+     * Starts the text animation with the provided text, duration, and animation listener.
+     *
+     * @param animText            The text to animate.
+     * @param duration            The duration of the animation in milliseconds.
+     * @param onAnimationListener A listener to handle animation start and end events.
+     */
     public void startAnimation(CharSequence animText, int duration, OnAnimationListener onAnimationListener) {
 
         // Use the provided text for animation
@@ -62,6 +96,10 @@ public class AnimatedText extends androidx.appcompat.widget.AppCompatTextView {
         textAnimator.start();
     }
 
+    /**
+     * Called when this view is detached from the window.
+     * Cancels the text animation if it is running.
+     */
     @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
@@ -70,10 +108,19 @@ public class AnimatedText extends androidx.appcompat.widget.AppCompatTextView {
         }
     }
 
+    /**
+     * Interface for handling animation start and end events.
+     */
     interface OnAnimationListener {
+        /**
+         * Callback when the animation starts.
+         */
         void onStartAnimation();
 
+        /**
+         * Callback when the animation ends.
+         */
         void onEndAnimation();
     }
-
 }
+
